1269f835db8b1b96982c5be041e56067490ad18f
atiktipu <mdatikulislamtipu@gmail.com>
2025-04-24 13:50:06 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const students = ["Emon", "Jahid", "Rakib", "Mira" ,"Meheraj"];
+students.map(name => console.log(`Welcome To (${name})`));
+
+
+
+const destination = ["Mohammodpur", "Mirpur", "Jatrabari", "Rampura","Baridhara"];
+destination.map(destination => console.log(`This is ${destination} destination`));
+
+
+
+const time = ["20 min", "40 min", "30 min", "50 min", "25 min"];
+time.map(time => console.log(`he/she is ${time} Late Everyday.`));
+
+
+
+const skills = ["C", "C++", "Java","JavaScript","Mongo DB"];
+skills.map(skill => console.log(`they are skilled in ${skill}`));
